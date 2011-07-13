6a72600a54516a5a0c99a75a8f3beead2f56eb28
francisco <francisco@francisco.(none)>
2011-07-13 02:26:08 +0100

    msm: kgsl: mark GPU wait time as IO time
    This change, coupled with marking IO time as busy time for cpufreq
    purposes, will improve performance for GPU bound graphics
    applications.
    
    Conflicts:
    
    	include/linux/wait.h

--- include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 22 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#22 PREEMPT Wed Jul 13 00:40:30 WEST 2011"
-#define LINUX_COMPILE_TIME "00:40:30"
-#define LINUX_COMPILE_BY "francisco"
-#define LINUX_COMPILE_HOST "francisco"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
