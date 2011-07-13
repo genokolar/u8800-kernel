5f3679304992599087738bd1d1736a8b747241d4
francisco <francisco@francisco.(none)>
2011-07-13 22:09:56 +0100

    Revert "ARM: 6750/1: improvements to compressed/head.S"
    This reverts commit 8ab59430183e595013020d1c1012e8a06f405845.

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 26 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#26 PREEMPT Wed Jul 13 03:59:42 WEST 2011"
+#define LINUX_COMPILE_TIME "03:59:42"
+#define LINUX_COMPILE_BY "francisco"
+#define LINUX_COMPILE_HOST "francisco"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
