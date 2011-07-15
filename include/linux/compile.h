9b8d330b4b7a9fffdf0f96c0194369e26b7a9e98
francisco <francisco@francisco.(none)>
2011-07-15 02:45:36 +0100

    msm_fb: display: Flush RGB pipe when solid fill mode is used. Timer.c and io.h backported from .35

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Fri Jul 15 01:01:31 WEST 2011"
-#define LINUX_COMPILE_TIME "01:01:31"
+#define UTS_VERSION "#7 PREEMPT Fri Jul 15 02:36:49 WEST 2011"
+#define LINUX_COMPILE_TIME "02:36:49"
 #define LINUX_COMPILE_BY "francisco"
 #define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILE_DOMAIN
